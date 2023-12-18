f9e93616616b186953219a03bbd4601f8752be38
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-12-18 13:25:39 -0300

    Feat - OrderStatus enum

--- src/main/java/com/anderson/orderControl/config/TestConfig.java
+++ src/main/java/com/anderson/orderControl/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.anderson.orderControl.config;
 
 import com.anderson.orderControl.entities.Order;
 import com.anderson.orderControl.entities.User;
+import com.anderson.orderControl.entities.enums.OrderStatus;
 import com.anderson.orderControl.repositories.OrderRepository;
 import com.anderson.orderControl.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Anderson", "anderson@gmail.com", "9999999", "123456");
         User u2 = new User(null, "Alexandre", "alexandre@gmail.com", "9888888", "678901");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
