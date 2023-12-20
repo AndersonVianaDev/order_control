b50d98758a3eda4c1a9c688308c3c5eb38acd015
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-12-20 13:39:23 -0300

    Feat - Update User

--- src/main/java/com/anderson/orderControl/controllers/UserController.java
+++ src/main/java/com/anderson/orderControl/controllers/UserController.java
@@ -42,4 +42,10 @@ public class UserController {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        User entity = userService.update(id, obj);
+        return ResponseEntity.ok().body(entity);
+    }
 }
