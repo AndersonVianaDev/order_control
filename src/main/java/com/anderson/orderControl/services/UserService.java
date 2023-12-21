28180e52ec977bb9ca48929aa735754acc407cd5
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-12-21 10:03:42 -0300

    Feat - Exception handling - findById

--- src/main/java/com/anderson/orderControl/services/UserService.java
+++ src/main/java/com/anderson/orderControl/services/UserService.java
@@ -2,6 +2,7 @@ package com.anderson.orderControl.services;
 
 import com.anderson.orderControl.entities.User;
 import com.anderson.orderControl.repositories.UserRepository;
+import com.anderson.orderControl.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
